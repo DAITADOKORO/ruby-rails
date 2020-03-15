20d7639da9b0f99e94f73f2dd472c8125a0306a1
DAITADOKORO <bangasa0101@gmail.com>
2020-03-15 23:29:20 +0900

    changed rspec関連

--- rspec関連.rb
+++ rspec関連.rb
@@ -58,9 +58,10 @@ require 'rails_helper'
 
 describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
-    #ここでlet! とすると、beforeの前にユーザーが登録できる。必須データの登録時に。
+    #let! とすると、beforeの前にユーザーが登録できる。必須データの登録時に。
     let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') }
     let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com') }
+    let!(:task_a) {FactoryBot.create(:task, title: 'テスト１', user: user_a) }
 
     before do
       FactoryBot.create(:task, name: '最初のタスク', user: user_a) # ここでユーザーAが登録される
@@ -89,3 +90,58 @@ describe 'タスク管理機能', type: :system do
 end
 
 # bundle exec rspec spec/system/tasks_spec.rb
+
+
+
+shared_examplesの活用
+
+
+require 'rails_helper'
+
+...
+
+  before do
+    FactoryBot.create(:task, name: '最初のタスク', user: user_a)
+    visit login_path
+    fill_in 'メールアドレス', with: login_user.email
+    fill_in 'パスワード', with: login_user.password
+    click_button 'ログインする'
+  end
+
+  shared_examples_for 'ユーザーAが作成したタスクが表示される' do # itの共通化
+    it { expect(page).to have_content '最初のタスク' }
+  end
+
+  describe '一覧表示機能' do
+    context 'ユーザーAがログインしているとき' do
+      let(:login_user) { user_a }
+
+      it behaves_like 'ユーザーAが作成したタスクが表示される' # shared_examplesからitを呼び出す
+    end
+
+  describe '詳細表示機能' do
+    context 'ユーザーAがログインしているとき' do
+      let(:login_user) { user_a }
+
+      before do
+        visit task_path(task_a)
+      end
+
+      it behaves_like 'ユーザーAが作成したタスクが表示される'
+    end
+  end
+end
+
+
+空白時のエラーチェックにletを使う
+
+describe '新規作成機能' do
+  let(:task_title) { 'テスト' } #デフォルトとして設定
+...
+
+  context '新規作成画面でタイトルを入力しなかった時' do
+    let(:task_tutke) { '' } # 上書き
+...
+  end
+end
+
